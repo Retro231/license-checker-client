418ecf4f74c5a9d19736aba1db018e3adc1d1bdf
Nazmus Shakib <nazshakib01@gmail.com>
2024-02-13 22:41:47 +0600

    merge search to master.

--- components/tab screens/Home.js
+++ components/tab screens/Home.js
@@ -1,17 +1,15 @@
 import {
-  FlatList,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { FlatList } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import Icons from "@expo/vector-icons/MaterialIcons";
 import Header from "../Header";
-import Search from "../Search";
 import { homeOptions } from "../../assets/api/homeOptions";
 import { companyHouseSearch } from "../../helper/companyHouseSearch";
 import { getSearchResult } from "../../helper/getSearchResult";
@@ -20,10 +18,12 @@ import { useSelector } from "react-redux";
 const Home = () => {
   const navigation = useNavigation();
   const [typedText, setTypedText] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState(null);
   const [loading, setLoading] = useState(false);
   const [notfound, setNotfound] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
+
+  const searchInputRef = useRef();
 
   const { activeOrg, newAddedOrg, removedOrg } = useSelector(
     (state) => state.app
@@ -66,12 +66,17 @@ const Home = () => {
     }
   };
 
+  const handleSearchBtnPress = () => {
+    handleSubmit();
+    searchInputRef.current.blur();
+  };
+
   const handleSelection = async (item) => {
     setLoading(true);
     const result = await companyHouseSearch(item);
     navigation.navigate("SearchResult", { result });
     setLoading(false);
-    setSuggestions([]);
+    setSuggestions();
   };
 
   const handleOptionPress = (caption) => {
@@ -81,56 +86,105 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Header />
+
+      {/* search start */}
       <View
         style={{
-          flex: isFocused ? 3 : 1,
+          backgroundColor: "white",
+          marginVertical: 36,
+          position: "relative",
           width: "100%",
-          alignItems: "center",
-          flexDirection: "column",
-          justifyContent: "flex-start",
+          zIndex: 1,
         }}
       >
-        <Search
-          handleTextChange={handleTextChange}
-          typedText={typedText}
-          handleFocus={handleFocus}
-          handleBlur={handleBlur}
-          handleSubmit={handleSubmit}
-        />
-        {isFocused && (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <TextInput
+            style={{
+              height: 40,
+              width: "65%",
+              paddingLeft: 10,
+              borderColor: "#039EBD",
+              borderWidth: 1,
+              borderTopLeftRadius: 30,
+              borderBottomLeftRadius: 30,
+            }}
+            ref={searchInputRef}
+            value={typedText}
+            onChangeText={handleTextChange}
+            onFocus={handleFocus}
+            placeholder="Search"
+            keyboardType="web-search"
+            enterKeyHint="search"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <TouchableOpacity
+            style={{
+              width: "15%",
+              height: 40,
+              backgroundColor: "#039EBD",
+              justifyContent: "center",
+              alignItems: "center",
+              borderTopEndRadius: 30,
+              borderBottomEndRadius: 30,
+              borderWidth: 0,
+            }}
+            onPress={handleSearchBtnPress}
+          >
+            <Icons name="search" size={26} />
+          </TouchableOpacity>
+        </View>
+        {!loading && notfound && (
           <View
             style={{
-              width: "100%",
-              alignItems: "center",
-              marginBottom: 45,
+              padding: 10,
             }}
           >
+            <Text style={{ textAlign: "center" }}>
+              No result found. Try something else.
+            </Text>
+          </View>
+        )}
+        <View
+          style={{
+            position: "absolute",
+            top: 41,
+            width: "100%",
+            display: `${suggestions || loading ? "flex" : "none"}`,
+            justifyContent: "center",
+          }}
+        >
+          {loading && (
             <View
               style={{
-                width: "65%",
-                backgroundColor: "#DDF3F8",
+                padding: 10,
               }}
             >
-              {!loading && notfound && (
-                <View style={{ padding: 10 }}>
-                  <Text style={{ textAlign: "center" }}>
-                    No result found. Try something else.
-                  </Text>
-                </View>
-              )}
-              {loading && (
-                <View style={{ padding: 10 }}>
-                  <Loading />
-                </View>
-              )}
+              <Loading />
+            </View>
+          )}
+          {suggestions && !loading && (
+            <View
+              style={{
+                height: 300,
+                marginHorizontal: 30,
+                borderWidth: 1,
+                borderRadius: 15,
+                borderColor: "#039EBD",
+                backgroundColor: "#cfe7ff",
+              }}
+            >
               {suggestions?.length !== 0 && (
                 <>
-                  <ScrollView
-                    showsHorizontalScrollIndicator={false}
-                    showsVerticalScrollIndicator={false}
-                    keyboardShouldPersistTaps="always"
-                  >
-                    {suggestions?.map((item, index) => (
+                  <FlatList
+                    data={suggestions}
+                    renderItem={({ item, index }) => (
                       <TouchableOpacity
                         key={index}
                         style={{
@@ -145,68 +199,71 @@ const Home = () => {
                           {item["Organisation Name"]}
                         </Text>
                       </TouchableOpacity>
-                    ))}
-                  </ScrollView>
+                    )}
+                  />
                 </>
               )}
             </View>
-          </View>
-        )}
-      </View>
-      {!isFocused && (
-        <View style={{ flex: 4 }}>
-          <FlatList
-            data={homeOptions}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item }) => (
-              <View
-                key={item.id}
-                style={[
-                  item.style,
-                  {
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    marginHorizontal: 20,
-                    paddingHorizontal: 18,
-                    paddingVertical: 36,
-                    borderRadius: 16,
-                    margin: 7,
-                  },
-                ]}
-              >
-                <View
-                  style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
-                >
-                  <Icons
-                    name={item.icon}
-                    size={24}
-                    style={{ color: item.color }}
-                  />
-                  <TouchableOpacity
-                    onPress={() => handleOptionPress(item.caption)}
-                  >
-                    <Text style={[styles.text, { fontWeight: 600 }]}>
-                      {item.title}
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-                <View
-                  style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
-                >
-                  <Text style={[styles.text]}>
-                    {item.caption === "active" && activeOrg.totalItem}
-                    {item.caption === "new" && newAddedOrg.totalItem}
-                    {item.caption === "remove" && removedOrg.totalItem}
-                  </Text>
-                  <Icons name={item.valueIndicatorUp} size={24} />
-                </View>
-              </View>
-            )}
-            keyExtractor={(item) => item.id}
-          />
+          )}
         </View>
-      )}
+      </View>
+      {/* search end */}
+
+      {/* home options */}
+      {/* {isFocused && ( */}
+      <View style={{ position: "relative", zIndex: 0 }}>
+        <FlatList
+          data={homeOptions}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View
+              key={item.id}
+              style={[
+                item.style,
+                {
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  marginHorizontal: 20,
+                  paddingHorizontal: 18,
+                  paddingVertical: 36,
+                  borderRadius: 16,
+                  margin: 7,
+                },
+              ]}
+            >
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+              >
+                <Icons
+                  name={item.icon}
+                  size={24}
+                  style={{ color: item.color }}
+                />
+                <TouchableOpacity
+                  onPress={() => handleOptionPress(item.caption)}
+                >
+                  <Text style={[styles.text, { fontWeight: 600 }]}>
+                    {item.title}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+              >
+                <Text style={[styles.text]}>
+                  {item.caption === "active" && activeOrg.totalItem}
+                  {item.caption === "new" && newAddedOrg.totalItem}
+                  {item.caption === "remove" && removedOrg.totalItem}
+                </Text>
+                <Icons name={item.valueIndicatorUp} size={24} />
+              </View>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      {/* )} */}
     </View>
   );
 };
